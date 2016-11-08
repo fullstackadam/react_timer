e28b31bebe3fd6492d2894cd673185f41e98d92b
Adam McMahon <obelesk411@users.noreply.github.com>
2016-11-08 10:15:12 -0700

    feat: add navbar and scaffold timer, countdown

---
+++ app/components/Nav.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+	return (
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="menu">
+					<li className="menu-text">React Timer</li>
+					<li><IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
+					<li><Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+				<ul className="menu">
+					<li className="menu-text">Created by <Link to="http://fullstackadam.com/" style={{display: 'inline', paddingLeft: 0}}>fullstackadam</Link></li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+module.exports = Nav;
